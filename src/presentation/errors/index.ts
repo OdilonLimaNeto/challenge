d89b0704940bd726b5d659367c9b0c2c2316c7dc
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-07-04 15:02:27 -0500

    feat: create errors usecases in presentation layer

---
+++ src/presentation/errors/index.ts
@@ -0,0 +1,7 @@
+export * from './access-denied-error'
+export * from './invalid-param-error'
+export * from './missing-param-error'
+export * from './missing-path-param-error'
+export * from './server-error'
+export * from './unauthorized-error'
+
