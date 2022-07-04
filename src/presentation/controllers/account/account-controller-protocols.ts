a40a364450a8cde15b2898b8fd58b0bb365bafa5
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-07-04 15:01:36 -0500

    feat: create account controller  in presentation layer

---
+++ src/presentation/controllers/account/account-controller-protocols.ts
@@ -0,0 +1,8 @@
+export * from '@/domain/models/account'
+export * from '@/domain/usecases/account/add-account'
+export * from '@/domain/usecases/account/find-account-by-id'
+export * from '@/domain/usecases/account/remove-account'
+export * from '@/domain/usecases/account/update-account'
+export * from '@/presentation/protocols'
+export * from '@/presentation/protocols/validations'
+
