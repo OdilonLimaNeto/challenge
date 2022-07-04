416e2feaeecf5931558030ec9ac14e3a73838abe
OdilonLimaNeto <odilongeronimoo@gmail.com>
2022-07-04 14:49:15 -0500

    feat: create new authentication usecase  in data layer

---
+++ src/data/usecases/authentication/db-authentication-protocols.ts
@@ -0,0 +1,6 @@
+export * from '@/data/protocols/cryptography/encrypter'
+export * from '@/data/protocols/cryptography/hash-comparer'
+export * from '@/data/protocols/db/account/find-account-by-email-repository'
+export * from '@/domain/usecases/authentication'
+export * from '../account/db-accounts-protocols'
+
---
+++ src/data/usecases/authentication/db-authentication.ts
@@ -0,0 +1,22 @@
+import { AddAccountsParams, Authentication, AuthResponse, Encrypter, FindAccountByEmailRepository, HashComparer } from './db-authentication-protocols'
+
+export class DbAuthentication implements Authentication {
+  constructor(
+    private readonly findAccountByEmailRepository: FindAccountByEmailRepository,
+    private readonly hashComparer: HashComparer,
+    private readonly encrypter: Encrypter
+  ) { }
+
+  async auth(authentication: AddAccountsParams): Promise<AuthResponse> {
+    const account = await this.findAccountByEmailRepository.findByEmail(authentication.email)
+    if (account) {
+      const isValid = await this.hashComparer.compare(authentication.password, account.password)
+      if (isValid) {
+        const { id, name, email } = account
+        const accessToken = await this.encrypter.encrypt(id)
+        return { user: { name, email, id }, accessToken }
+      }
+    }
+    return null
+  }
+}
